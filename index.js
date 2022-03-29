1119c4ec159193e3fd02c6708ec51e42c9769855
bcaudillo <btcaudillo@gmail.com>
2022-03-29 11:01:03 -0600

    commit tree 87262f96d52f60877f72c5727d62ca976958b364

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+let cats =["Milo","Otis","Garfield"]
+
+function destructivelyAppendCat(name){
+    return cats.push(name)
+}
+
+function destructivelyPrependCat(name){
+    return cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift()
+}
+
+function appendCat(name){
+    let cats2 = [...cats,name]
+    return cats2
+}
+
+function prependCat(name){
+    let cats3 = [name,...cats]
+    return cats3
+}
+
+function removeLastCat(){
+    let cats4 = cats.slice(0,-1)
+    return cats4
+}
+
+function removeFirstCat(){
+    let cats5 = cats.slice(1)
+    return cats5
+}
